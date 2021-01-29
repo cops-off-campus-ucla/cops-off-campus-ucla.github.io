84755224602f1aacd262a26e5fc28e7db8f5a018
Cops off Campus UCLA <78193573+cops-off-campus-ucla@users.noreply.github.com>
2021-01-29 11:03:48 -0800

    Add files via upload

---
+++ backgrounds.js
@@ -0,0 +1,47 @@
+var backgrounds = [
+  {
+    img: 'map',
+    startSlug: 'slide-0',
+    endSlug: 'slide-5'
+  },
+  {
+    img: 'https://i.imgur.com/eGKEN3Z.jpg',
+    startSlug: 'slide-6',
+    endSlug: 'slide-6'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-7',
+    endSlug: 'slide-12'
+  },
+  {
+    img: 'https://i.imgur.com/SyrbUI4.jpg',
+    startSlug: 'slide-13',
+    endSlug: 'slide-13'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-14',
+    endSlug: 'slide-14'
+  },
+  {
+    img:'https://i.imgur.com/fNB7GQQ.jpg',
+    startSlug:'slide-15',
+    endSlug:'slide-15'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-16',
+    endSlug: 'slide-18'
+  },
+  {
+    img:'https://i.imgur.com/5o14xg1.jpg',
+    startSlug:'slide-19',
+    endSlug:'slide-19'
+  },
+  {
+    img: 'map',
+    startSlug: 'slide-20',
+    endSlug: 'slide-21'
+  }
+]
