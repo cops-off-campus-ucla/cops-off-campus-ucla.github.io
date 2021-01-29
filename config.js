f844711b3436e98c7cbf5eadd7b81fb01e78ef5c
Cops off Campus UCLA <78193573+cops-off-campus-ucla@users.noreply.github.com>
2021-01-29 01:10:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -357,8 +357,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Gayley Avenue, November 2013',
-            image: 'cunning.png',
-            imageCredit: '<a href="https://ballotpedia.org/David_S._Cunningham,_III" target="_blank">Ballotpedia</a>',
+            image: 'cunningham.jfif',
+            imageCredit: '<a href="https://www.latimes.com/local/lanow/la-xpm-2013-nov-25-la-me-ln-black-judge-ucla-police-20131125-story.html" target="_blank">Ken Hively</a>',
             description: 'Judge David S. Cunningham III, a former president of the Los Angeles Police Commision, was pulled over by two UCPD officers on Gayley Avenue near <a href="https://dailybruin.com/2013/11/25/la-judge-files-excessive-force-complaint-against-ucpd" target="_blank">Whole Foods Market</a>. He was pulled over for not wearing a seatbelt and was handcuffed for resisting arrest, and was shoved into the back of the police car. UCPD were found to have used racially-motivated excessive force, and Cunningham won a <a href="https://dailybruin.com/2014/07/11/la-judge-uc-reach-settlement-over-10m-damages-claim-against-ucpd" target="_blank">$500,000 settlement</a>. ',
             location: {
                 center: [-118.44749, 34.06118 ],
